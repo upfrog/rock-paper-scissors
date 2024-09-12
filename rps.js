268f151e2ee1fc5102a4ab0edb5f91c937d4cac7
Stephen Rout <srr@routhouse.org>
2024-09-12 16:44:56 -0400

    Implement playRound

--- rps.js
+++ rps.js
@@ -1,8 +1,14 @@
+let humanScore = 0;
+let computerScore = 0;
 
 console.log("starting!");
-console.log(getHumanChoice());
-console.log(getComputerChoice());
+
+
+playRound(getHumanChoice(), getComputerChoice());
+
 console.log("done!");
+
+
 
 
 function getComputerChoice()
@@ -13,16 +19,17 @@ function getComputerChoice()
     while (computerChoice == "")
     {
         let choiceSeed = Math.random();
-        
+        console.log(choiceSeed);
         if (choiceSeed < 0.33) {
-            computerChoice = "Rock";
+            computerChoice = "rock";
         }
         else if (choiceSeed < 0.66) {
-            computerChoice = "Paper";
+            computerChoice = "paper";
         }
         else if (choiceSeed < 0.99) {
-            computerChoice = "Scissors";
+            computerChoice = "scissors";
         }
+        console.log(computerChoice);
         //if choiceSeed is above .99, roll again - I don't want one of the options to be chosen 34% of the time!
     }
 
@@ -33,13 +40,81 @@ function getHumanChoice() {
     let humanChoice = "";
     
     while (humanChoice == "") {
-        humanChoice = prompt("Please enter \"Rock\", \"Paper\", or \"Scissors\".");
+        humanChoice = prompt("Please enter \"Rock\", \"Paper\", or \"Scissors\".").toLowerCase();
 
-        if (humanChoice != "Rock" && humanChoice != "Paper" && humanChoice != "Scissors") {
+        if (humanChoice != "rock" && humanChoice != "paper" && humanChoice != "scissors") {
             humanChoice = "";
             console.log("I'm sorry, that was not a valid input. Please try again.");
         }
     }
 
     return humanChoice;
+}
+
+
+function playRound(humanChoice, computerChoice) {
+    //I want to find a more elegant way of doing this - perhaps assign each of the choices a numerical value, and 
+    //map different outcomes to different results when those numerical values are input to some function?
+    //Alternatively, I could use an object, and give each choice a weakTo or strongTo. 
+    let result = 0; //0 = draw, 1 = human wins, 2 = computer wins
+
+    if (humanChoice == "rock") {
+        if (computerChoice == "rock") {
+            //draw
+            result = 0;
+        }
+        else if (computerChoice == "paper") {
+            //lose
+            result = 2;
+        }
+        else {
+            //win
+            result = 1;
+        }
+    }
+    else if (humanChoice == "paper") {
+        if (computerChoice == "rock") {
+            //win
+            result = 1;
+        }
+        else if (computerChoice == "paper") {
+            //draw
+            result = 0;
+        }
+        else {
+            //lose
+            result = 2;
+        }
+    }
+    else if (humanChoice == "scissors") {
+        if (computerChoice == "rock") {
+            //lose
+            result = 2;
+        }
+        else if (computerChoice == "paper") {
+            //win
+            result = 1;
+        }
+        else {
+            //draw
+            result = 0;
+        }
+    }
+
+    switch(result) {
+        case 0:
+            console.log("That's a draw.");
+            break;
+        case 1:
+            console.log("You win this round! Congratulations!");
+            ++humanScore;
+            break;
+        case 2:
+            console.log("You lose this round.");
+            ++computerScore;
+            break;
+    }
+
+
+
 }
